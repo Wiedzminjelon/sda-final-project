22640b521256361553519474b5dffa665a956081
Marcin <marcinskalski1993@gmail.com>
2020-09-27 12:30:00 +0200

    addition of base structure sugestion and H2 database configuration

---
+++ src/main/java/socialmediaapp/twitterinspiredapp/enums/VOTE_TYPE.java
@@ -0,0 +1,9 @@
+package socialmediaapp.twitterinspiredapp.enums;
+
+public enum VOTE_TYPE {
+    UPVOTE(1),
+    DOWNVOTE(-1);
+
+    VOTE_TYPE(int direction){}
+
+}
---
+++ src/main/java/socialmediaapp/twitterinspiredapp/model/Comment.java
@@ -0,0 +1,33 @@
+package socialmediaapp.twitterinspiredapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.time.Instant;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentId;
+
+    @NotEmpty
+    private String text;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "postId", referencedColumnName = "postId")
+    private Post post;
+    private Instant createdDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private User user;
+
+}
