dabf6259a4718d21899147b25a07818324f287da
Ewa <strojna.ew@gmail.com>
2020-10-03 09:49:59 +0200

    frontend base + header tests (not working yet, no errors, looking for solution)

--- src/main/resources/frontend/twitter-angular-app/src/app/app.module.ts
+++ src/main/resources/frontend/twitter-angular-app/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { EditUserComponent } from './edit-user/edit-user.component';
+import { UserComponent } from './user/user.component';
+import { CreatePostComponent } from './create-post/create-post.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogInComponent,
+    SignUpComponent,
+    EditUserComponent,
+    UserComponent,
+    CreatePostComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
